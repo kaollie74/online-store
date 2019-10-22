112ebe37e834ffb2e4005e72be47f09bd641aed7
kaollie74 <kaollie74@gmail.com>
2019-10-22 17:44:43 -0500

    added routes for each component in App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+
+// import components
+import Home from "../Home/Home";
+import Signin from '../Signin/Signin';
+import Signup from '../Signup/Signup';
+import Checkout from '../Checkout/Checkout';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>blah</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Redirect  exact from = "/" to = "/home" />
+          <Route component={Home} path="/home" />
+          <Route component={Signin} path="/signin" />
+          <Route component={Signup} path="/signup" />
+          <Route component={Checkout} path="/checkout" />
+        </Switch>
+      </Router>
+    )
+  }
+
+
+
 }
 
 export default App;
