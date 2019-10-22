3e58cbf9f2be7436170b023f6dea2ecb9f33f53c
kaollie74 <kaollie74@gmail.com>
2019-10-22 18:55:50 -0500

    inserted logo and have it aligned center in the nav bar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Text, Heading } from 'gestalt';
+import { Box, Text, Heading, Image } from 'gestalt';
 import { NavLink } from 'react-router-dom';
 
 
@@ -8,9 +8,9 @@ const Navbar = () => {
   return (
     <>
       <Box
-        display = "flex"
-        alignItems = "center"
-        justifyContent= "around"
+        display="flex"
+        alignItems="center"
+        justifyContent="around"
         height={70}
         color="midnight"
         padding={1}
@@ -18,19 +18,31 @@ const Navbar = () => {
       >
         {/*************** Sign in Link ************************ */}
         <NavLink to="/signin" >
-          <Text size = "x1" color="white">Sign In</Text>
+          <Text size="x1" color="white">
+            Sign In
+          </Text>
         </NavLink>
 
         {/****** Title and Logo *****/}
-        <NavLink to ="/">
-          <Heading size="xs" color="orange">
-            BrewHaha
+        <NavLink to="/">
+          <Box  display='flex' alignItems='center'  >
+            <Box margin={3} height={50} width={50}>
+              <Image
+                alt="BrewHaha Logo"
+                naturalHeight={1}
+                naturalWidth={1}
+                src='./image/beer.png'
+              />
+            </Box>
+            <Heading size="xs" color="orange">
+              BrewHaha
           </Heading>
+          </Box>
         </NavLink>
 
         {/*************** Sign up Link ************************ */}
         <NavLink to="/signup" >
-          <Text size = "x1" color="white">Sign Up</Text>
+          <Text size="x1" color="white">Sign Up</Text>
         </NavLink>
 
       </Box>
